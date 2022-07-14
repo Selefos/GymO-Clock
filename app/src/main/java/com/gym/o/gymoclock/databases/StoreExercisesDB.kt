a6fd106a9995803764af3989c28d73720d3c60de
Selefos <andrgiann92@gmail.com>
2022-07-14 03:04:07 +0300

    Removed extra files

--- app/src/main/java/com/gym/o/gymoclock/databases/StoreExercisesDB.kt
+++ app/src/main/java/com/gym/o/gymoclock/databases/StoreExercisesDB.kt
@@ -1,5 +1,6 @@
 package com.gym.o.gymoclock.databases
 
 import android.content.Context
+//Saves Screen of Exercises done
 
-class StoreExercisesDB(context: Context?)
+class StoreExercisesDB(context: Context?){}
